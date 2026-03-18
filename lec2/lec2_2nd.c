75a65c8ca00e3904b35084ee2d2efda973755073
Eunseo Yu <eunseo0@yonsei.ac.kr>
2026-03-18 04:49:11 +0000

    새로운 C 프로그램 파일 추가

---
+++ lec2/lec2_2nd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 6;
+    if ((-5 <= a) && (a < 4)) {
+        printf("Print 1 run!!\n");
+        printf("Print 1 run!!\n");
+    }
+
+    return 0;
+}
